eec52b4dae1c6b66f9f643eb40c4554796d2707c
CrossmodalGroup <htzhang_codes@163.com>
2022-07-30 11:54:39 +0800

    Update testall.py

--- testall.py
+++ testall.py
@@ -11,15 +11,10 @@ from vocab import Vocabulary
 import evaluation
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-# RUN_PATH = "/mnt/data10t/bakuphome20210617/zhangkun/neg_2021_10_7/runs/runX/checkpoint/checkpoint_16_523.2.pth.tar"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# RUN_PATH = "/mnt/data10t/bakuphome20210617/zhangkun/neg_2021_10_7/runs/runX/checkpoint/checkpoint_16_523.2.pth.tar"
+RUN_PATH = ""
 
-RUN_PATH = "/mnt/data10t/bakuphome20210617/zhangkun/neg_2021_10_22/runs/runX/checkpoint/model_best.pth.tar"
+DATA_PATH = ""
 
-
-
-DATA_PATH = "/mnt/data10t/bakuphome20210617/zhangkun/data/"
-# evaluation.evalrank(RUN_PATH, data_path=DATA_PATH, split="dev",fold5=False)
 evaluation.evalrank(RUN_PATH, data_path=DATA_PATH, split="testall",fold5=False)
